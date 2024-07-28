5ca3ea5603cf411d3654c70315049b468710e0f0
unknown <user@vm>
2024-07-28 18:15:12 -0400

    DLL creates console

--- dll/dll.cpp
+++ dll/dll.cpp
@@ -1,14 +1,53 @@
 #include <windows.h>
+#include <string>
+#include <iostream>
+
+void setup_console() {
+    AllocConsole();
+
+    FILE *fDummy;
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+
+    std::cout.clear();
+    std::cin.clear();
+    std::cerr.clear();
+}
+
+void on_dll_attach()
+{
+    setup_console();
+
+    PWSTR thread_desc = NULL;
+    HRESULT hr = GetThreadDescription(GetCurrentThread(), &thread_desc);
+    if (FAILED(hr))
+    {
+        std::string msg = "Couldn't get thread description" + std::to_string(hr);
+        MessageBoxA(NULL, msg.c_str(), NULL, MB_OK);
+
+        ExitProcess(1);
+    }
+
+    printf("Main thread is: %ls\n", thread_desc);
+
+    LocalFree(thread_desc);
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
 
     switch (fdwReason)
     {
-        case DLL_PROCESS_ATTACH:
-            MessageBoxA(NULL, "Hello World!", NULL, MB_OK);
-        default:
-            break;
+    case DLL_PROCESS_ATTACH:
+    {
+        on_dll_attach();
+        break;
+    }
+    default:
+    {
+        break;
+    }
     }
 
     return true;
--- loader/main.cpp
+++ loader/main.cpp
@@ -1,10 +1,59 @@
 #include <windows.h>
+#include <string>
 #include <cstdio>
 
 // TODO: don't hardcode paths
 const char *GAME_CWD = "Z:\\TGAAC-for-research\\";
 const char *GAME_PATH = "Z:\\TGAAC-for-research\\TGAAC.exe.unpacked-for-renderdoc.exe";
 const char *DLL_NAME = "C:\\Users\\User\\Desktop\\tgaamod\\build\\dll\\Debug\\tgaamod.dll";
+
+bool inject_dll_to_process(const char *dll_to_inject, HANDLE process, HANDLE main_thread)
+{
+    // Suspend main thread so we our DLL can setup before the application runs
+    SuspendThread(main_thread);
+
+    HMODULE kernel32_lib = LoadLibraryA("kernel32.dll");
+    // Apparently kernel32.dll should have the same base address across different processes, so this will work fine.
+    LPVOID address_of_LoadLibraryA = GetProcAddress(kernel32_lib, "LoadLibraryA");
+
+    LPVOID remote_dll_name = VirtualAllocEx(process, nullptr, strlen(dll_to_inject) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (remote_dll_name == NULL)
+    {
+        printf("Couldn't allocate DLL name string in remote process: %d", GetLastError());
+        return false;
+    }
+
+    if (WriteProcessMemory(process, remote_dll_name, dll_to_inject, strlen(dll_to_inject) + 1, NULL) == 0)
+    {
+        printf("Couldn't write DLL name string to remote process: %d", GetLastError());
+        return false;
+    };
+
+    HANDLE remote_thread = CreateRemoteThread(process, nullptr, 0, static_cast<LPTHREAD_START_ROUTINE>(address_of_LoadLibraryA), remote_dll_name, CREATE_SUSPENDED, nullptr);
+    if (remote_thread == NULL)
+    {
+        printf("Couldn't create thread in remote process: %d", GetLastError());
+        return false;
+    }
+
+    // Pass in the main thread id, so the DLL can resume it after setup
+    DWORD remote_main_thread_id = GetThreadId(main_thread);
+    std::wstring remote_thread_description = std::to_wstring(remote_main_thread_id);
+    HRESULT hr = SetThreadDescription(remote_thread, remote_thread_description.c_str());
+    if (FAILED(hr))
+    {
+        printf("Couldn't set remote thread description: %d", hr);
+        return false;
+    }
+
+    if (ResumeThread(remote_thread) == -1)
+    {
+        printf("Couldn't resume remote thread: %d", GetLastError());
+        return false;
+    };
+
+    return true;
+}
 
 int main()
 {
@@ -33,10 +82,23 @@ int main()
             return 0;
             break;
         }
+
         case CREATE_PROCESS_DEBUG_EVENT:
         {
-            // TODO
-            break;
+            CREATE_PROCESS_DEBUG_INFO *create_info = &debug_event.u.CreateProcessInfo;
+            printf("Process launch event [baddr: %p thread: %d]\n", create_info->lpBaseOfImage, GetThreadId(create_info->hThread));
+
+            if (!inject_dll_to_process(DLL_NAME, create_info->hProcess, create_info->hThread))
+            {
+                printf("Failed to inject DLL into process, killing\n");
+                TerminateProcess(process_info.hProcess, 0);
+
+                // Let us handle the exit process event so we get a nice exit message (stupid)
+                break;
+            };
+
+            DebugActiveProcessStop(process_info.dwProcessId);
+            return 0;
         }
 
         default:
